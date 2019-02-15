9a7fd7ee07163db856d71a1df54261d48c28e57b
ivankalita <45788320+ivankalita@users.noreply.github.com>
2019-02-15 23:31:02 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+
+#-- На этой стадии мы определямся с количеством обрабатываемых html-страниц
+url_const = "https://escholarship.org"
+url = "https://escholarship.org/search?campuses=ucb&departments=iber_resin&departments=citris&departments=crest&departments=cedr&disciplines=Engineering&disciplines=Physical+Sciences+and+Mathematics&type_of_work=article"
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+nav = soup.find('nav', class_='c-pagination--next').find('li', class_='c-pagination__next')
+count = nav.previous_sibling.get_text()
+print(count)
+url_1 = 'https://escholarship.org/search?campuses=ucb&departments=iber_resin&departments=citris&departments=crest&departments=cedr&disciplines=Engineering&disciplines=Physical+Sciences+and+Mathematics&start='
+url_2 = '&type_of_work=article'
+i = 10 #--переменная для контакенации нового urlа
+iter = 1 #--переменная для названия нового документа
+#---------------------------------------#
+#--Отсюда начинается цикл обхода всех ссылок с дейтсвующими download-ами на каждой html-странице
+while i < 30: #(int(count)+1)*10: Боевой цикл на скачивание 406 PDF-документа 
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    heading = soup.find_all('heading')
+    for head in heading:
+        href = head.find('a').get('href')
+        r = requests.get(url_const + href)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        href_dow = soup.find('div', class_='o-download').find('a').get('href')
+        print(href_dow)
+    #Download PDF-file
+        pdf = requests.get(href_dow)
+        f = open("pdf/{}.pdf".format(iter), "wb")
+        f.write(pdf.content)
+        f.close()
+        iter += 1
+        print(iter)
+        url = url_1 + str(i) + url_2
+    i += 10
+
+
+
+
+
+#section = soup.find('section', class_='o-columnbox1').find_all('section', class_='c-scholworks')
+#for sec in section:
+#    href = section.find('heading').find('a').get('href')
+#print(heading)
+
+
